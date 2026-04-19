257d02d1af34abdff38456ee8f278ee47f6f8f49
Alexander Slobodskoj <alexslobodskoj@gmail.com>
2026-04-19 14:52:15 +0300

    Add startup_pipeline_checkdb DAG with db availability check

---
+++ airflow/dags/startup_pipeline_checkdb.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    dag_id='startup_pipeline_checkdb',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='0 2 * * *',
+    catchup=False,
+    description='dbt pipeline with db check'
+) as dag:
+
+    check_db = BashOperator(
+        task_id='check_db',
+        bash_command=(
+            'pg_isready -h localhost -p 5432 -U alexslobodskoj -d postgres'
+        ),
+        execution_timeout=timedelta(minutes=10)
+    )
+
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command=(
+            'source /Users/alexslobodskoj/Documents/GitHub/Portfolio/dbt-project/dbt-env/bin/activate && '
+            'cd /Users/alexslobodskoj/Documents/GitHub/Portfolio/dbt-project/startups && '
+            'dbt run 2>&1'
+        ),
+        execution_timeout=timedelta(minutes=10)
+    )
+
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command=(
+            'source /Users/alexslobodskoj/Documents/GitHub/Portfolio/dbt-project/dbt-env/bin/activate && '
+            'cd /Users/alexslobodskoj/Documents/GitHub/Portfolio/dbt-project/startups && '
+            'dbt test 2>&1'
+        ),
+        execution_timeout=timedelta(minutes=10)
+    )
+
+    check_db >> dbt_run >> dbt_test
